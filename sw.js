e4b29a10de7d47195bc6ac538d842b385983ad4b
Cesario Angelino <cesario.angelino001@studenti.uniparthenope.it>
2022-12-30 19:13:11 +0100

    Modified files

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./images/logo.png"]);
+            return cache.addAll(["./", "./bootstrap/dist/css/bootstrap.min.css", "./images/logo_192.png"]);
         })
     );
 });
